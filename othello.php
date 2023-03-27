abfcd27239bb2187b822968b57ab3c98ad25debf
gashishishi <wawwooaz@gmail.com>
2023-03-27 19:15:30 +0900

    jsファイルを追加

--- othello.php
+++ othello.php
@@ -108,22 +108,7 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-<script>
-  let board = new Array();
-  for(let i = 0; i < 8; i++){
-    board.push(new Array(8));
-  }
-  console.log(board);
-      jQuery(function($){
-        $('.grid').on('click',function(event){
-          let grid = $(this).attr('id');
-          $('#' + grid).html('<div class="stone"></div>');
-          let num = parseInt(grid.substr(5),10);
-          let i = parseInt(num / 8, 10);
-          let j = num % 8;
-          console.log(board[i][j]);
-        });
-      });
+<script src="js/jsfile.js">
 
     </script>
 
